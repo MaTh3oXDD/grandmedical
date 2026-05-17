ac6c28232f44b16b0a46efe1dd7ebd27327dce3a
251536 Jdrzejczak Mateusz <251536@edu.p.lodz.pl>
2026-05-17 20:49:54 +0200

    chore: add basePath for GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/grand-medical-clinic',
+  assetPrefix: '/grand-medical-clinic',
   images: {
     unoptimized: true,
   },
