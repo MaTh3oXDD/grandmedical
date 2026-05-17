99a83833f16f1d3aa349a9ab970997fd096af561
251536 Jdrzejczak Mateusz <251536@edu.p.lodz.pl>
2026-05-17 18:49:38 +0200

    Updates

--- _next/static/bZAUsyRucIFvYS8DOC5yh/_buildManifest.js
+++ _next/static/bZAUsyRucIFvYS8DOC5yh/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/grandmedical//_next/:path+",
-        "destination": "/grandmedical/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
